d150faadb516c08e6db7aba2166500b418d53786
Lazlo <lopezslazarus@gmail.com>
2021-07-28 21:15:58 -0700

    General Updates

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello-world></hello-world>
+    <book-list></book-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import BookList from './components/BookList';
+import BookList from './components/BookList.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    BookList
+    BookList,
   },
 };
 </script>
